95857dac1645d686df7e0ab35adb4746b6f80399
Carlos Mario Perdomo Ramos <cmperdomo@unicauca.edu.co>
2024-06-10 21:29:22 -0500

    chore: Update mysql package to mysql2 for improved performance

--- src/database.ts
+++ src/database.ts
@@ -1,13 +1,13 @@
-import mysql from 'mysql2/promise';
+import { createPool } from 'mysql2/promise';
 import keys from './keys';
 
-const pool = mysql.createPool(keys.database);
+const pool = createPool(keys.database);
 
 (async () => {
     try {
         const connection = await pool.getConnection(); 
-        connection.release(); 
         console.log('DB is connected');
+        connection.release(); 
     } catch (error) {
         console.error('Error connecting to the database:', error);
         process.exit(1); 
