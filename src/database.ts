5426f6e56f0712cb6f0b7e4d68c199e6ae0ba155
Carlos Mario Perdomo Ramos <cmperdomo@unicauca.edu.co>
2024-06-10 21:23:27 -0500

    updat mysql protocol auth

--- src/database.ts
+++ src/database.ts
@@ -1,12 +1,17 @@
-import mysql from 'mysql2';
+import mysql from 'mysql2/promise';
 import keys from './keys';
 
 const pool = mysql.createPool(keys.database);
 
-pool.getConnection((err, connection)=>{
-    if (err) throw err; 
-    connection.release();
-    console.log('DB is connected')
-});
+(async () => {
+    try {
+        const connection = await pool.getConnection(); 
+        connection.release(); 
+        console.log('DB is connected');
+    } catch (error) {
+        console.error('Error connecting to the database:', error);
+        process.exit(1); 
+    }
+})();
 
 export default pool;
